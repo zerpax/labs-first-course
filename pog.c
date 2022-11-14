20e2065cb2fff532f54cffbf7e791097b7cc956e
zerpax <nik_fedorov_2004@mail.ru>
2022-11-14 20:31:31 +0300

    fixed lab9

--- pog.c
+++ pog.c
@@ -1,54 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int max(int a, int b){
-    if(a > b) return a;
-    else return b;
+int Max(int a, int b) {
+    return a > b ? a : b;
 }
 
-int min(int a, int b){
-    if(a < b) return a;
-    else return b;
+int Min(int a, int b) {
+    return a < b ? a : b;
 }
 
-int abs(int a){
+int Abs(int a) {
     if(a >= 0) return a;
     else return -1 * a;
 }
 
-int sign(int a){
+int Sign(int a) {
     if(a > 0) return 1;
     if(a == 0) return 0;
     if(a < 0) return -1;
 }
 
-int modulo(int a, int b){
+int Modulo(int a, int b) {
     if (a % b == 0) return a / b;
     else if (a * b > 0) return a % b;
     else return a - (a/b-1) * b;
-    }
+}
 
-int main(){
+int main() {
     int i = 20, j = 0, l = 11;
     bool check = false;
 
-    for(int k = 0; k < 50; k++){
-        if(i >= -10 && j >= 0 && j <= -i+10){
+    for (int k = 0; k < 50; k++) {
+        if (i >= -10 && j >= 0 && j <= -i+10){
             printf("Hit\n steps:%d i=%d j=%d l=%d\n", k, i, j, l);
             check = true;
             break;
         }
 
-        i = modulo((i - k)*max(j, l) + (j - k)*min(i, l) + (l - k)*max(i, j), 23);
-        j = modulo(-((i - k)*min(j, l) + (j-k)+max(i, l) + (l - k)*min(i, j)), 27);  
-        l = abs(i + j - l - k)*sign(i - j + l - k);
+        i = Modulo((i - k)*Max(j, l) + (j - k)*Min(i, l) + (l - k)*Max(i, j), 23);
+        j = Modulo(-((i - k)*Min(j, l) + (j-k)+Max(i, l) + (l - k)*Min(i, j)), 27);  
+        l = Abs(i + j - l - k)*Sign(i - j + l - k);
     }
-    if(!check){
-        if(i >= -10 && j >= 0 && j <= -i+10){
+    if (!check) {
+        if (i >= -10 && j >= 0 && j <= -i+10) {
                 printf("Hit\n steps:%d i=%d j=%d l=%d\n", 50, i, j, l);
                 check = true;
             }
     }
 
-    if(!check) printf("Miss\n steps:%d i=%d j=%d l=%d\n", 51, i, j, l);
+    if (!check) printf("Miss\n steps:%d i=%d j=%d l=%d\n", 51, i, j, l);
 }
