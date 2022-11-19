a80016461a16ecccd1817d16e121aa78c35db2a2
zerpax <nik_fedorov_2004@mail.ru>
2022-11-19 14:25:57 +0300

    fixed lab13.c

--- lab13.c
+++ lab13.c
@@ -2,40 +2,38 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#define vowels (1u << ('a' - 'a') | 1u << ('e' - 'a') | 1u << ('i' - 'a') | 1u << ('o' - 'a') | 1u << ('u' - 'a'))
+#define VOWELS (1u << ('a' - 'a') | 1u << ('e' - 'a') | 1u << ('i' - 'a') | 1u << ('o' - 'a') | 1u << ('u' - 'a'))
 
 bool IsVowel(char c) {
-	return (1u << (tolower(c) - 'a') & vowels) != 0;
+  return (1u << (tolower(c) - 'a') & VOWELS) != 0;
 }
 
 int main() {
-	char c, prev;
-	bool check = false, s = false;
+  char c, prev;
+  bool check = false, s = false;
 
-	c = getchar();
-	prev = c;
+  c = getchar();
+  prev = c;
 
-	s = IsVowel(c);
+  s = IsVowel(c);
 
-	while (c != EOF) {
-		if (c == ' ' || c == ',') {
-			s = s & IsVowel(prev);
-            check = s | check;	
-            
-            c = getchar();
-            s = IsVowel(c); 
-		}	
-        if (c == '\n') {
-            s = s & IsVowel(prev);
-            check = s | check;	
-            if (check) {                
-                printf("Yes\n");        
-            } else {
-                printf("No\n");            
-            }
-        }
-        
-		prev = c;
-		c = getchar();
+  while (c != EOF) {
+    if (c == ' ' || c == ',') {
+      s = s & IsVowel(prev);
+      check = s | check;	    
+      c = getchar();
+      s = IsVowel(c); 
+    }	
+    if (c == '\n') {
+      s = s & IsVowel(prev);
+      check = s | check;	
+      if (check) {                
+        printf("Yes\n");        
+      } else {
+        printf("No\n");            
+      }
+    }
+    prev = c;
+    c = getchar();
 	}
 }
