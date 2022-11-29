96f3c658c9597b331ab63fb06580c0cb96dc403f
zerpax <nik_fedorov_2004@mail.ru>
2022-11-29 22:11:35 +0300

    fixed lab12.c

--- lab12.c
+++ lab12.c
@@ -13,16 +13,15 @@ long long Shift(long long n) {
     long long w = 1, z = 1;
     int zeros = 0, digs = 0;
     while (n % w != n) {
-				w *= 10;
-				digs++;
-				if (n % (z*10) == 0) {
-					z *= 10;	
-					zeros++;
-				}
-			}
-	
-			long long half = Pow(10, (digs - zeros) / 2);
-			n = (n - n % (z * half) + (n / z % half));
+	    w *= 10;
+	    digs++;
+	    if (n % (z*10) == 0) {
+		    z *= 10;	
+    		    zeros++;
+	    }
+    }	
+    long long half = Pow(10, (digs - zeros) / 2);
+    n = (n - n % (z * half) + (n / z % half));
     return n;
 }
 
@@ -30,27 +29,25 @@ long long Shift(long long n) {
 
 int main() {
 	long long n = 0;
-    int sign = 1;	
-    char c;
+	int sign = 1;	
+	char c;
 	c = getchar();
-
-    if (c == '-')   {
-        sign = -1;
-        c = getchar();
-    }
-
+	if (c == '-') {
+		sign = -1;
+		c = getchar();
+	}
 	while (c != EOF) {
-		if (c == '\n'){
+		if (c == '\n') {
 			n = Shift(n);
-            n *= sign;
-            printf("%lld\n", n);
+			n *= sign;
+			printf("%lld\n", n);
 			n = 0;
 		} else if (c == '-') {
-                sign = -1;
-        } else {
-		n *= 10;
-		n += (int)(c - '0');
-        } 
-    c = getchar();
+			sign = -1;
+		} else {
+			n *= 10;
+			n += (int)(c - '0');
+		} 
+		c = getchar();
 	}
 }	
