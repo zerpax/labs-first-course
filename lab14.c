d4c98782a6bc52df2de72f708639c304e4e60061
zerpax <nik_fedorov_2004@mail.ru>
2022-12-02 13:37:35 +0300

    fixed lab14.c

--- lab14.c
+++ lab14.c
@@ -3,14 +3,15 @@
 int main(){
 	char c = getchar();
 	int m[7][7];
-	int row = 0, col = 0, num = 0;
+	int row = 0, col = 0, num = 0, sign = 1;
 	while (c != EOF) {
 		if (c == ' ') {
-			m[row][col] = num;
+			m[row][col] = num * sign;
 			++col;
 			num = 0;
+			sign = 1;
 		} else if (c == '\n') {
-			m[row][col] = num;
+			m[row][col] = num * sign;
 			if (col == row) {
 				for (int i = 0; i <= row * 2; ++i) {
 					for (int j = i < row ? i : row; j >= 0 && j >= i - row; --j) {
@@ -27,7 +28,10 @@ int main(){
 			++row;
 			col = 0;
 			num = 0;
-		} else {
+			sign = 1;
+		} else if (c == '-') {
+			sign = -1;
+		}else {
 			num = num * 10 + ((int)c - 48);
 		}
 		c = getchar();
