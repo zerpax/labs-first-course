93bb1fedfe5987c2f35c23565aa5f60f721567db
zerpax <nik_fedorov_2004@mail.ru>
2023-05-06 09:32:26 +0300

    fixed kp9.c

--- kp9.c
+++ kp9.c
@@ -2,16 +2,13 @@
 #include <stdbool.h>
 
 typedef struct Entry{
-	int key[4];
+	int key;
 	char value[50];
 } Entry;
 
 void Print(Entry* a, int n) {
 	for (int i = 0; i < n; i++) {
-                for (int j = 0; j < 4; j++) {
-                        printf("%d ", a[i].key[j]);
-                }
-		//printf("%d ", a[i].key);
+		printf("%d ", a[i].key);
                 printf("%s\n", a[i].value);
         }
 
@@ -26,42 +23,19 @@ bool Equal(char* a, char* b){
 	return true;
 }
 
-bool Qgt (int* a, int* b) {
-	for (int i = 0; i < 4; i++) {
-		if (a[i] > b[i]) {
-			return true;
-		}
-		if (a[i] < b[i]) {
-			return false;
-		}
-	}
-	return false;
-}
 
-bool Qeq(int* a, int* b) {
-        for (int i = 0; i < 4 ; i++) {
-                if (a[i] != b[i]) {
-                        return false;
-                }
-        }
-        return true;
-}
-
-
-
-char* Search(Entry *a, int size, int* x) {
+char* Search(Entry *a, int size, int x) {
 	int R = size;
 	int L = -1;
 	while (R - L > 1) {
-		//printf("%d %d\n", R, L);
-		if (Qgt(x, a[(R + L) / 2].key)) {
+		if (x > a[(R + L) / 2].key) {
 			L = (R + L) / 2; 
 		}
 		else {
 			R = (R + L) / 2;
 		}
 	}
-	if (Qeq(x, a[R].key)) {
+	if (x == a[R].key) {
 		return a[R].value;
 	}
 	else {
@@ -81,7 +55,7 @@ void Sort(Entry* a, int size){
 	int right_size  = 0;
 	
 	for (int i = 0; i < size - right_size; i++) {
-		while (Qgt(a[i].key, pivot.key)/*a[i].key > pivot.key*/ && i <= size - right_size - 1) {
+		while (a[i].key > pivot.key && i <= size - right_size - 1) {
 		
 			Entry temp = a[i];
 			a[i] = a[size - right_size - 1];
@@ -109,8 +83,7 @@ int main() {
 	scanf("%d", &n);
 	Entry a[50];
 	for (int i = 0; i < n; i++) {
-		//scanf("%d %[^\n]value", &a[i].key, a[i].value);
-		scanf("%d %d %d %d %[^\n]value", &a[i].key[0], &a[i].key[1], &a[i].key[2], &a[i].key[3], a[i].value);
+		scanf("%d %[^\n]value", &a[i].key, a[i].value);
 	}
 	Print(a, n);
 
@@ -123,8 +96,8 @@ int main() {
 	int m;
        	scanf("%d", &m);	
 	for (int i = 0; i < m; i++) {
-		int key[4];
-		scanf("%d %d %d %d", &key[0], &key[1], &key[2], &key[3]);
+		int key;
+		scanf("%d", &key);
 		printf("%s\n", Search(a, n, key));
 	}
 }
